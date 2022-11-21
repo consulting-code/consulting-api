966222b962fba68775e0c33b64fb48b1281c4a94
zero <zeropersonalitydisorder@gmail.com>
2022-11-21 20:47:10 +0100

    init commit

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+// Body parser
+app.use(express.json());
+
+// Enable CORS
+app.use(cors());
+
+app.use("/api/v1/greeting", (req, res) =>
+  res.status(200).json({
+    success: true,
+  })
+);
+
+const PORT = process.env.PORT | 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
